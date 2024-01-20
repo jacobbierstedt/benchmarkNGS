e7f5128420d0182e7ec4f25482e03ad86d2eb0a5
jacobbierstedt <jacob.r.bierstedt@gmail.com>
2024-01-20 16:27:29 -0600

    minimal read trimmer

--- apps/app_trim_reads.cpp
+++ apps/app_trim_reads.cpp
@@ -1,12 +1,80 @@
 #include <iostream>
+#include <getopt.h>
+#include <string>
+#include <zlib.h>
 #include "Reader.hpp"
 #include "trim.hpp"
 
 
 
 
+static int readLength         = 0;
+static int trim3              = 0;
+static int trim5              = 0;
+static int minLen             = 0;
+static float minProp          = 0.0;
+static bool discardShorties   = false;
+static std::string inputFile  = "";
+static std::string outputFile = "";
 
-int main(int argc, char const *argv[]) {
-  // WIP
+void usage(int xc) {
+  fprintf(stderr, "Usage: trim_reads [options] -i <input_file> -o <output_file>\n\n");
+  fprintf(stderr, "Required arguments:\n");
+  fprintf(stderr, "-i str      input fastq/a (gzipped or not gzipped)\n");
+  fprintf(stderr, "-o str      output gzipped fastq\n\n");
+  fprintf(stderr, "Options:\n");
+  fprintf(stderr, "-h          Display usage message\n");
+  fprintf(stderr, "-l int      trim reads to overall length\n");
+  fprintf(stderr, "-t int      bp to trim from right\n");
+  fprintf(stderr, "-f int      bp to trim from left\n");
+  fprintf(stderr, "-m int      minimum length to trim reads to\n");
+  fprintf(stderr, "-p float    minimum proprotion of read to retain\n");
+  fprintf(stderr, "-d          discard reads that fail to meet length minimums\n");
+  exit(xc);
+}
+
+void parse_command_line(int argc, char ** argv) {
+  int c;
+  while ((c = getopt(argc, argv, "hl:t:f:m:p:di:o:")) != -1) {
+    switch(c) {
+      case 'h': usage(0);                              break;
+      case 'l': readLength      = atoi(optarg);        break;
+      case 't': trim3           = atoi(optarg);        break;
+      case 'f': trim5           = atoi(optarg);        break;
+      case 'm': minLen          = atoi(optarg);        break;
+      case 'p': minProp         = atof(optarg);        break;
+      case 'd': discardShorties = true;                break;
+      case 'i': inputFile       = std::string(optarg); break;
+      case 'o': outputFile      = std::string(optarg); break;
+      default: usage(1);                               break;
+    }
+  }
+  if (inputFile.length() <= 2) {
+    std::cerr << "Option -i requires a valid file path: " << inputFile << std::endl; exit(1);
+  }
+  if (outputFile.length() <= 2) {
+    std::cerr << "Option -o requires a valid file path: " << outputFile << std::endl; exit(1);
+  }
+}
+
+void trimReads() {
+  Trimmer trimmer  = Trimmer(readLength, trim3, trim5, minLen, minProp, discardShorties);
+  Reader  seqs     = Reader(inputFile);
+  gzFile  output   = gzopen(outputFile.c_str(), "w");
+  SeqRead read;
+  while (seqs.iterSequences(read) != -1) {
+    trimmer.trimRead(read);
+    if (read.keep) {
+      int len = 0;
+      std::string outread = read.toFastq(len);
+      gzwrite(output, outread.c_str(), len);
+    }
+  }
+  gzclose(output);
+}
+
+int main(int argc, char ** argv) {
+  parse_command_line(argc, argv);
+  trimReads();
   return 0;
 }
