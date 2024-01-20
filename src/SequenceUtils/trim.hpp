ac1a8a7bd64a2bee1db4c96a5f0fd22b89ddddc9
jacobbierstedt <jacob.r.bierstedt@gmail.com>
2024-01-20 16:33:05 -0600

    more trimmer params and members

--- src/SequenceUtils/trim.hpp
+++ src/SequenceUtils/trim.hpp
@@ -9,18 +9,30 @@
 
 class Trimmer {
 public:
-  int readLength;
-  int trim3;
-  int trim5;
+  int   readLength;
+  int   trim3;
+  int   trim5;
+  int   minLen;
+  float minProp;
+  bool  discardByLength;
 
   Trimmer() {};
   Trimmer(int readLength);
   Trimmer(int readLength, int trim3, int trim5);
+  Trimmer(
+    int   readLength,
+    int   trim3,
+    int   trim5,
+    int   minLen,
+    float minProp,
+    bool  discardByLength
+  );
   virtual ~Trimmer () {};
 
   void trimReadToLength(SeqRead & read);
   void hard3Trim(SeqRead & read);
   void hard5Trim(SeqRead & read);
+  void trimRead(SeqRead & read);
 };
 
 #endif // trim.hpp
